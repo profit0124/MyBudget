487b02ff0e271d08cdb2fd5915b9a04fa2d2d3c0
profit <profit0124@gmail.com>
2025-04-16 22:26:36 +0900

    [Feature] add CategoryBudget, Budget

---
+++ MyBudget/MyBudget/Model/Budget.swift
@@ -0,0 +1,28 @@
+//
+//  Budget.swift
+//  MyBudget
+//
+//  Created by Sooik Kim on 4/16/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Budget {
+    @Attribute(.unique) var id: UUID = UUID()
+    var month: Date
+    var totalLimit: Double?
+    var isCustom: Bool = false
+    var categoryBudgets: [CategoryBudget]
+
+    init(month: Date,
+         totalLimit: Double? = nil,
+         isCustom: Bool = false,
+         categoryBudgets: [CategoryBudget] = []) {
+        self.month = month
+        self.totalLimit = totalLimit
+        self.isCustom = isCustom
+        self.categoryBudgets = categoryBudgets
+    }
+}
